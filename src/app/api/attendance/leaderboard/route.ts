126914cf5c912752f79ada31f7652a5927040137
shivapreetham <2005shivapreetham@gmail.com>
2025-04-03 17:36:39 +0530

    avatar field removed from user

--- src/app/api/attendance/leaderboard/route.ts
+++ src/app/api/attendance/leaderboard/route.ts
@@ -31,7 +31,6 @@ export async function GET(request: Request) {
         email: true,
         batch: true,
         branch: true,
-        avatar: true,
         course: true,
         lastSeen: true,
         overallPercentage: true,
